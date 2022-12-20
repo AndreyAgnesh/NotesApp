dbee50ed68d7447593f057d5c3b10fed3bf5bc32
AndreyAgnesh <Agnesha28g@yandex.by>
2022-12-21 01:41:48 +0300

    Step_2. Create view components on screens (JetPack Compose)

--- app/src/main/java/com/example/notesapp/screens/Note.kt
+++ app/src/main/java/com/example/notesapp/screens/Note.kt
@@ -1,9 +1,65 @@
 package com.example.notesapp.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.notesapp.ui.theme.NotesAppTheme
 
 @Composable
-fun Note(navController: NavHostController) {
+fun NoteScreen(navController: NavHostController) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(28.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "title",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    Text(
+                        text = "subtitle",
+                        fontSize = 16.sp,
+                        modifier = Modifier.padding(top = 6.dp)
+                    )
+                }
+            }
+        }
+    }
+}
 
+@Composable
+@Preview(showBackground = true)
+fun prevNoteScreen() {
+    NotesAppTheme {
+        NoteScreen(navController = rememberNavController())
+    }
 }
